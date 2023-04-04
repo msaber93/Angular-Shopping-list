f4eeb8a10475b4ce1e1acd6c6837fc3514fc04be
Mahmoud Saber <engabosaber2014@gmail.com>
2023-04-04 02:13:13 +0200

    Creating The Components

---
+++ src/app/recipes/recipe-detailes/recipe-detailes.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeDetailesComponent } from './recipe-detailes.component';
+
+describe('RecipeDetailesComponent', () => {
+  let component: RecipeDetailesComponent;
+  let fixture: ComponentFixture<RecipeDetailesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecipeDetailesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RecipeDetailesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/recipes/recipe-detailes/recipe-detailes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detailes',
+  templateUrl: './recipe-detailes.component.html',
+  styleUrls: ['./recipe-detailes.component.css']
+})
+export class RecipeDetailesComponent {
+
+}
